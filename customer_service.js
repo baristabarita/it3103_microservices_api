69016b64756392dac93ca722467299b1e4e44741
Shane Louis Barita <89013136+baristabarita@users.noreply.github.com>
2024-09-16 20:05:57 +0800

    [FIX] Fixed and modified order_service.js

--- customer_service.js
+++ customer_service.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3002;
 
 app.use(express.json());
 
--- product_service.js
+++ product_service.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3001;
 
 app.use(express.json());
 
