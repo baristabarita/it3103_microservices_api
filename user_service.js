a12287f6d8bd61eb915b3e81124c53d653f6a270
GrdnhffMann <138700965+LJynver@users.noreply.github.com>
2024-10-05 03:57:13 +0800

    [feat] imported jwt, console log testing the authentication tokens

--- user_service.js
+++ user_service.js
@@ -1,6 +1,7 @@
 //user Service index file
 
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3002;
 
@@ -9,8 +10,24 @@ app.use(express.json());
 let users = [];
 let userIdCounter = 1;
 
-//Add New user
+function generateToken(user) {
+    const payload = {
+        id: user.id,
+        user: user.name,
+        email: user.email,
+        pass: user.pass
+    }
+    console.log(payload);
+    return jwt.sign(payload, 'secret', { expiresIn: '1h' });
+}
+
+//Add New user, Also known as Signups
 app.post('/users', (req, res) => {
+
+    const { name, email, pass } = req.body;
+    console.log(`Name of the user: ${name}`);
+    console.log(`Email of the user: ${email}`);
+    console.log(`Password of the user: ${pass}`);
 
     const newuser = req.body;
     newuser.id = userIdCounter++;
@@ -22,7 +39,30 @@ app.post('/users', (req, res) => {
         res.status(500).json({error: "Error adding new user"});
     }
 
-    console.log(users);
+    // console.log(users);
+
+});
+
+//Login User
+app.post('/login', (req, res) => {
+    
+    const { id, name, email, pass } = req.body;
+
+    console.log(`Incoming Id: ${id}`);
+    console.log(`Incoming Name: ${name}`);
+    console.log(`Incoming Email: ${email}`);
+    console.log(`Incoming Password: ${pass}`);
+
+    const incomingUser = users.find(u => u.email === email && u.pass === pass);
+    
+    if (incomingUser) {
+        const token = generateToken(incomingUser);
+        res.json({ token });
+    } else {
+        res.status(401).json({
+            error: 'Invalid email or password'
+        });
+    }
 
 });
  
